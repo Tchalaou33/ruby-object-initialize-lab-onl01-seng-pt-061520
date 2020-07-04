fecc11963b1ce8e3e03b958c878f61087c2ed57d
Andrea Tchalaou <attchalaou@gmail.com>
2020-07-04 19:59:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,19 @@
-class Dog
-  def initialize(name, breed="Mutt")
-    @name=name
-    @breed=breed
-  def name=(name, breed="Mutt")
-    @name=name
-    @breed=breed
-  end
-  def name
-    @name
-  end
-  def breed
-    @breed
-end
-end
+# class Dog
+#   def initialize(name, breed="Mutt")
+#     @name=name
+#     @breed=breed
+#   def name=(name, breed="Mutt")
+#     @name=name
+#     @breed=breed
+#   end
+#   def name
+#     @name
+#   end
+#   def breed
+#     @breed
+# end
+# end
 
-sam=Dog.new("Poddle")
-sam.name
-sam.breed
+# sam=Dog.new("Poddle")
+# sam.name
+# sam.breed
